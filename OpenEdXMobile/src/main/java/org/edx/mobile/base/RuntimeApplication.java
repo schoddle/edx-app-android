613feab05655d423e58d65ffc7c41873117a70bc
schoddle <109148950+schoddle@users.noreply.github.com>
2022-07-19 10:47:36 +0000

    Update RuntimeApplication.java

--- OpenEdXMobile/src/main/java/org/edx/mobile/base/RuntimeApplication.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/base/RuntimeApplication.java
@@ -1,5 +1,18 @@
 package org.edx.mobile.base;
 
+// adMob related
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.RequestConfiguration;
+import com.google.android.gms.ads.initialization.AdapterStatus;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+import java.util.Arrays;
+import java.util.Map;
+
 /**
  * Put any custom application configuration here.
  * This file will not be edited by edX unless absolutely necessary.
@@ -9,6 +22,30 @@ public class RuntimeApplication extends MainApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        // If you have any custom extensions, add them here.
+
+        // initialize AdMob
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+                Map<String, AdapterStatus> statusMap = initializationStatus.getAdapterStatusMap();
+                for (String adapterClass : statusMap.keySet()) {
+                    AdapterStatus status = statusMap.get(adapterClass);
+                    Log.d("MiniSchool", String.format(
+                            "Adapter name: %s, Description: %s, Latency: %d",
+                            adapterClass, status.getDescription(), status.getLatency()));
+                    Toast.makeText(this, "MobileAds initialized").show();
+                }
+
+
+            }
+        });
+
+        //TO-DO : change device number
+        MobileAds.setRequestConfiguration(
+                new RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
+                        .build());
+
     }
+
+
 }
